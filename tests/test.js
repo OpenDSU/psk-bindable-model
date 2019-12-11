4be174358c7fe593bd6555a020bb96e8ee632c44
Mastaleru Rafael <mastaleru.rafael@gmail.com>
2019-12-11 18:27:12 +0200

    fixes: triggering too many events, wait for the entire object subtree to be changed before raising change events
    added some tests

--- tests/test.js
+++ tests/test.js
@@ -1,10 +1,15 @@
 const assert = require("../../double-check").assert;
 const data = require("./data.json");
 const BindableModel = require("../lib/PskBindableModel.js");
-let testData = Object.assign({}, data);
-let model = BindableModel.setModel(testData);
+
+function cleanModel(){
+    return JSON.parse(JSON.stringify(data));
+}
 
 assert.callback("simple chain test", (done) => {
+    let testData = cleanModel();
+    delete testData['favoriteBooks'];
+    let model = BindableModel.setModel(testData);
 
     let changesCount = 0;
     const changes = [{chain: "name", value: "Rafael"}, {chain: "email", value: "raf@rms.ro"}];
@@ -26,6 +31,9 @@ assert.callback("simple chain test", (done) => {
 });
 
 assert.callback("simple model change test", (done) => {
+    let testData = cleanModel();
+    delete testData['favoriteBooks'];
+    let model = BindableModel.setModel(testData);
 
     let changesCount = 0;
     const changes = [{chain: "name", value: "Rafael"}, {chain: "email", value: "raf@rms.ro"}];
@@ -41,40 +49,69 @@ assert.callback("simple model change test", (done) => {
         });
         model[change.chain] = change.value;
     }
-
     changes.forEach(makeModelChangeTest);
+});
+
+assert.callback("wildcard test", (done) => {
+
+    let testData = cleanModel();
+    delete testData['favoriteBooks'];
+    let model = BindableModel.setModel(testData);
+
+    let expectedChanges = ["name.label", "birthdate.value", "name.label","birthdate.value.month"];
+    let changesCount = expectedChanges.length;
+
+    model.onChange("*", function (changedChain) {
+
+        assert.notEqual(expectedChanges.indexOf(changedChain), -1, "Chain was already removed");
+        expectedChanges.splice(expectedChanges.indexOf(changedChain), 1);
+
+        changesCount --;
+        if(changesCount === 0  && expectedChanges.length ===0){
+            done();
+        }
+    });
+
+    model.name.label="Mastaleru";
+    model.setChainValue("birthdate",{
+        value: {
+            "day": "1",
+            "month": "January",
+            "year": 2000
+        }
+    });
+
+    model.setChainValue("name.label","R.Mastaleru");
+    model.birthdate.value.month = "December";
 
 });
 
+
 assert.callback("multiple chain model change test", (done) => {
 
+    let testData = cleanModel();
+    delete testData['favoriteBooks'];
+    let model = BindableModel.setModel(testData);
 
-        model.onChange("name", function(changedChain){
-            console.log(changedChain)
-            assert.equal(changedChain, "name.label");
-            done();
+    let chainChangesExpected = ["name", "name.label", "name.text", "name.meta.data", "name.meta"];
+
+    let expectedEvents = chainChangesExpected.length;
+    chainChangesExpected.forEach((chain) => {
+        model.onChange(chain, function (changedChain) {
+
+            //after the change event is received, we remove the chain to be sure that the event
+            // is not triggered multiple times for a single change
+
+            assert.equal(changedChain, chain, "Chains should be equals");
+            assert.notEqual(chainChangesExpected.indexOf(changedChain), -1, "Chain was already removed");
+            chainChangesExpected.splice(chainChangesExpected.indexOf(changedChain), 1);
+            expectedEvents--;
+            if (expectedEvents === 0 && chainChangesExpected.length === 0) {
+                done();
+            }
         });
+    });
 
-        model.onChange("name.label", function(changedChain){
-            //console.log(changedChain)
-            assert.equal(changedChain, "name.label");
-           // done();
-        });
-
-        //model.name.label = "Alexandru";
-        setTimeout(()=>{
-            model.name={
-                "label": "First name",
-                "required": true,
-                "value": "",
-                "placeholder": "Enter your name here",
-                "meta":{
-                    type:"input",
-                    length:"200"
-                }
-            };
-        },100)
-
-
+    model.name = {"label": "First name", "text": "value", meta: {"data": "Rafael"}};
 
 });
